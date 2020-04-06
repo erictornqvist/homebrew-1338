7d478de8e36320851b8c432070afffec24ffee3f
Eric Törnqvist <ericto@kth.se>
2020-04-06 18:06:27 +0200

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,25 @@
+name = "Krampus Sack Drop Rate"
+description = "Gimme dat sack" 
+author = "Eric"
+version = "0.0.1"
+
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+-- Specify compatibility with the game!
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+dst_compatible = true
+all_clients_require_mod = false
+
+server_filter_tags = {
+"krampus",
+"sack"
+}
+
---
+++ modmain.lua
@@ -0,0 +1,19 @@
+AddPrefabPostInit("krampus", function(inst)
+	local SetSharedLootTable = GLOBAL.SetSharedLootTable
+	SetSharedLootTable( 'krampus',
+	{
+		{'monstermeat',  1.0},
+		{'charcoal',     1.0},
+		{'charcoal',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'nightmarefuel',     1.0},
+		{'krampus_sack', 0.33},
+	})
+  end
+)
